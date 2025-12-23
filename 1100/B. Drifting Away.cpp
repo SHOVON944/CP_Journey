dd0d181b4f3fb7ca1d880bcddc0e2221dd09b170
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-23 10:58:19 +0600

    add

--- 1100/B. Drifting Away.cpp
+++ 1100/B. Drifting Away.cpp
@@ -12,10 +12,28 @@ int main()
 
         bool valid = false;
         for(int i=0; i<n-1; i++){   // ** er jnno
-            if(s[i]=='*'  &&  s[i+1]=='*'){
+            if(s[i]=='*'  &&  s[i+1]=='<'){
                 cout<<-1<<endl;
                 valid = true;
                 break;
+            }
+        }
+        if(!valid){
+            for(int i=0; i<n-1; i++){   // ** er jnno
+                if(s[i]=='>'  &&  s[i+1]=='*'){
+                    cout<<-1<<endl;
+                    valid = true;
+                    break;
+                }
+            }
+        }
+        if(!valid){
+            for(int i=0; i<n-1; i++){   // ** er jnno
+                if(s[i]=='*'  &&  s[i+1]=='*'){
+                    cout<<-1<<endl;
+                    valid = true;
+                    break;
+                }
             }
         }
         if(!valid){
@@ -37,8 +55,22 @@ int main()
             }
         }
         if(!valid){
-            int max_step = count(s.begin(), s.end(), '*');  // finite case er jnno
-            cout<< (n + max_step - 1)<<endl;
+            int ans = 0;
+            for(int i=0; i<n; i++){
+                if(s[i]=='<'  ||  s[i]=='*'){
+                    ans = i + 1;
+                } else{
+                    break;
+                }
+            }
+            for(int i=n-1; i>=0; i--){
+                if(s[i]=='>'  ||  s[i]=='*'){
+                    ans = max(ans, (n-i));
+                } else{
+                    break;
+                }
+            }
+            cout<<ans<<endl;
         }
     }
 
