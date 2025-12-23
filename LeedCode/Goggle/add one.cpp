75e2bc42dcc7a565fcc7540cb2c2e762f12c1931
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-24 00:16:25 +0600

    add

---
+++ LeedCode/Goggle/add one.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        long long n = digits.size();
+        for(int i=n-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            } else{
+                digits[i] = 0;
+            }
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
