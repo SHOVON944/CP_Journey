55654bdb77196aa87b3a3a893f2600968373c48c
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-29 16:05:40 +0600

    add

---
+++ LeedCode/Codee/3434.maximum-frequency-after-subarray-operation.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3434 lang=cpp
+ *
+ * [3434] Maximum Frequency After Subarray Operation
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
