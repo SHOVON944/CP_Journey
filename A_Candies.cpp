629c037197d8f5bae83a92be007c429e91053a44
MD. SAKIBUL ISLAM SHOVON <mdsakibulislamshovon944@gmail.com>
2026-05-12 23:58:19 +0600

    add

---
+++ A_Candies.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        for(int k = 2; k <= 30; k++){
+            int sum = (1 << k) - 1;
+
+            if(n % sum == 0){
+                cout<<n / sum<<endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
